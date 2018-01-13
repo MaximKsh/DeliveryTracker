4f28fd64eb236436cdcc7880375d16bd4a0ac9e7
Kashirin Maxim <ksh.max@gmail.com>
2018-01-14 00:00:14 +0300

    скрипт очистки базы и расширения в миграцию

---
+++ backend/SQL/clear_db.py
@@ -0,0 +1,24 @@
+# pip3 install psycopg2
+
+import psycopg2
+
+
+
+if __name__ == '__main__':
+    with open('connection_string', 'r') as connection_string_file:
+        connection_string = connection_string_file.read()
+
+    conn = psycopg2.connect(connection_string)
+    cur = conn.cursor()
+
+    cur.execute(""" select tablename from pg_tables where "schemaname" = 'public';""")
+    queries = []
+    for row in cur:
+        queries.append('drop table ' + row[0] + ' cascade;\n')
+    full_query = ''.join(queries)
+    print(full_query)
+    cur.execute(full_query)
+     
+    conn.commit()
+    cur.close()
+    conn.close()
